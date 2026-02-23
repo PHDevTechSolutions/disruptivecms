0d22d6065d3d59debfdb8f6cfa2ff9cbe1c2f74c
Aries Jace <ariesjaceu@gmail.com>
2026-02-23 17:44:18 +0800

    add

--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -1,12 +1,21 @@
 import admin from "firebase-admin";
-import serviceAccount from "./adminsdk.json";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      serviceAccount as admin.ServiceAccount,
-    ),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_ADMIN_TYPE,
+      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+      token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_CERT,
+      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_CERT,
+    } as any), // ✅ cast to any
   });
 }
 
 export const adminDb = admin.firestore();
+export default admin;
